7e46fe41463b2fc523718f8c489bd8ac557246f0
mckinsta <morekoushal@gmail.com>
2026-04-29 04:19:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,28 @@
 import json
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+from telegram.ext import Updater, MessageHandler, Filters
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 with open("movies.json", "r") as f:
     movies = json.load(f)
 
-async def search_movie(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def search_movie(update, context):
     user_text = update.message.text.lower()
-
-    results = []
 
     for movie in movies:
         if user_text in movie["name"].lower():
-            results.append(movie)
-
-    if results:
-        for movie in results[:3]:
-            await update.message.reply_text(
+            update.message.reply_text(
                 f"{movie['name']} ({movie['year']})\nWatch: {movie['link']}"
             )
-    else:
-        await update.message.reply_text("Movie nahi sapadli bhau 😅")
+            return
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT, search_movie))
+    update.message.reply_text("Movie nahi sapadli bhau 😅")
 
-app.run_polling()
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(MessageHandler(Filters.text, search_movie))
+
+updater.start_polling()
+updater.idle()
